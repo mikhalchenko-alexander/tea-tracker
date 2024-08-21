ddf24eecdb289630835348900d3b296919fae0ca
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-08-21 14:55:01 +0300

    Use double quotes everywhere.

--- tea-tracker-flet/main.py
+++ tea-tracker-flet/main.py
@@ -1,12 +1,12 @@
 import flet as ft
 
 color = {
-    'green_main': '#318653',
-    'green_light': '#71BD85',
-    'orange_light': '#FAD074',
-    'orange': '#FFC634',
-    'light': '#FFC634',
-    'black': '#272726'
+    "green_main": "#318653",
+    "green_light": "#71BD85",
+    "orange_light": "#FAD074",
+    "orange": "#FFC634",
+    "light": "#FFC634",
+    "black": "#272726"
 }
 
 def main(page: ft.Page):
@@ -15,7 +15,7 @@ def main(page: ft.Page):
     }
     page.window_width = 390
     page.window_height = 844
-    page.bgcolor = color['black']
+    page.bgcolor = color["black"]
     page.update()
 
 
