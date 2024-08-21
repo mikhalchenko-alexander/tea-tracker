8714557b23d55c826c6602b1fcd3fb39b0b07938
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-08-21 16:31:20 +0300

    Add appbar.

--- tea-tracker-flet/main.py
+++ tea-tracker-flet/main.py
@@ -1,21 +1,45 @@
 import flet as ft
 
-color = {
-    "green_main": "#318653",
-    "green_light": "#71BD85",
-    "orange_light": "#FAD074",
-    "orange": "#FFC634",
-    "light": "#FFC634",
-    "black": "#272726"
-}
+from styling.styles import Font, Color
+
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
-    page.fonts = {
-    }
-    page.window_width = 390
-    page.window_height = 844
-    page.bgcolor = color["black"]
+    page.fonts = {font: font for font in Font}
+    page.add(
+        ft.SafeArea(
+            ft.Pagelet(
+                appbar=ft.AppBar(
+                    bgcolor=Color.BLACK,
+                    center_title=True,
+                    leading=ft.IconButton(
+                        icon=ft.icons.ARROW_BACK_IOS,
+                        icon_color=Color.ORANGE_LIGHT
+                    ),
+                    title=ft.Text(
+                        value="New set",
+                        color=Color.ORANGE_LIGHT,
+                        font_family=Font.INKNUT_ANTIQUA,
+                        size=20
+                    ),
+                    actions=[
+                        ft.PopupMenuButton(
+                            items=[],
+                            icon_color=Color.ORANGE_LIGHT,
+                            icon=ft.icons.MORE_VERT
+                        )
+                    ],
+                ),
+                content=ft.Container(
+                    border=ft.Border(
+                        top=ft.BorderSide(width=1, color=Color.ORANGE_LIGHT_TRANSPARENT),
+                        bottom=ft.BorderSide(width=1, color=Color.ORANGE_LIGHT_TRANSPARENT),
+                    ),
+                    height=50
+                )
+            )))
+    page.window.width = 390
+    page.window.height = 844
+    page.bgcolor = Color.BLACK
     page.update()
 
 
---
+++ tea-tracker-flet/styling/styles.py
@@ -0,0 +1,16 @@
+from enum import StrEnum
+
+
+class Color(StrEnum):
+    GREEN_MAIN = "#318653",
+    GREEN_LIGHT = "#71BD85",
+    ORANGE_LIGHT = "#FAD074",
+    ORANGE_LIGHT_TRANSPARENT = "#99FAD074",
+    ORANGE = "#FFC634",
+    LIGHT = "#FFC634",
+    BLACK = "#272726"
+
+
+class Font(StrEnum):
+    INKNUT_ANTIQUA = "fonts/InknutAntiqua-Regular.ttf"
+    INKNUT_ANTIQUA_BOLD = "fonts/InknutAntiqua-Bold.ttf"
